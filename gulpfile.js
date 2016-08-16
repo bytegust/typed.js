52eb05b25f307163adf94b57669e65fcf602cf41
Doğan Çelik <dogancelik93@gmail.com>
2016-08-16 03:09:48 +0300

    Put nomin in dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ var gulp = require('gulp')
     rename = require('gulp-rename')
     uglify = require('gulp-uglify');
 
- 
+
 gulp.task('compress', function() {
   gulp.src('js/*.js')
+    .pipe(gulp.dest('dist'))
     .pipe(uglify())
     .pipe(rename('typed.min.js'))
     .pipe(gulp.dest('dist'))
